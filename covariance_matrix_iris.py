b3e7ace8ae336a9a5a7188428bf823485d8c2b4e
praveendareddy21 <praveendareddy@gmail.com>
2017-07-18 19:58:38 -0700

    added covariance matrix example on iris dataset.

---
+++ covariance_matrix_iris.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+import numpy as np
+iris = datasets.load_iris()
+cov_data = np.corrcoef(iris.data.T)
+print (iris.feature_names)
+
+print (cov_data)
+import matplotlib.pyplot as plt
+img = plt.matshow(cov_data, cmap=plt.cm.winter)
+plt.colorbar(img, ticks=[-1, 0, 1])
+
+plt.show()
